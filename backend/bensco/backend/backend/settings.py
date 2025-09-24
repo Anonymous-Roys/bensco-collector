e71bd282c769440b88b23d83d54783ac75a8d7a5
Arhin David Kwabena <davidarhin2005@gmail.com>
2025-09-24 15:28:02 +0000

    email

--- backend/bensco/backend/backend/settings.py
+++ backend/bensco/backend/backend/settings.py
@@ -177,9 +177,9 @@ SIMPLE_JWT = {
 ## Email Settings
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 465  # safer than 465 on Render
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = "davidarhin2005@gmail.com"
 EMAIL_HOST_PASSWORD = "fiwy cfmf zntp uhcy"
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
